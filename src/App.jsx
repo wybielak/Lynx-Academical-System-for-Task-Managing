f3c3381e239961e833f450b27469ff9580ece214
Dawid <31769799+wybielak@users.noreply.github.com>
2024-04-14 13:56:03 +0200

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Lynx</h1>
+        <h2>Akademicki System Zarządzania Zadaniami</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
