d6337b2a43b876509cbf0d594f5f6fb087188a74
Dawid <31769799+wybielak@users.noreply.github.com>
2024-05-09 22:49:14 +0200

    api pliki

---
+++ back/lynxFileAPI/lynxFileAPI/Controllers/FileManagerController.cs
@@ -0,0 +1,36 @@
+﻿using lynxFileAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lynxFileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileManagerController : ControllerBase
+    {
+        //https://www.youtube.com/watch?v=bN64VNISNw0
+
+        private readonly IManageImage _iManageImage;
+        public FileManagerController(IManageImage iManageImage)
+        {
+            _iManageImage = iManageImage;
+        }
+
+        [HttpPost]
+        [Route("uploadfile")]
+        public async Task<IActionResult> UploadFile(IFormFile _IFormFile)
+        {
+            var result = await _iManageImage.UploadFile(_IFormFile);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("downloadfile")]
+        public async Task<IActionResult> DownloadFile(string FileName)
+        {
+            var result = await _iManageImage.DownloadFile(FileName);
+            return File(result.Item1, result.Item2, result.Item2);
+        }
+
+    }
+}
---
+++ back/lynxFileAPI/lynxFileAPI/Services/IManageImage.cs
@@ -0,0 +1,8 @@
+﻿namespace lynxFileAPI.Services
+{
+    public interface IManageImage
+    {
+        Task<String> UploadFile(IFormFile _IFormFile);
+        Task<(byte[], string, string)> DownloadFile(string FileName);
+    }
+}
